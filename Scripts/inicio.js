920b39d60684a33452382afc20194c9267b85df1
Alex117-Tech <alexseneta117@gmail.com>
2025-01-03 20:32:42 +0845

    Add files via upload

---
+++ Scripts/inicio.js
@@ -0,0 +1,27 @@
+ 
+ function mostrareserva() {
+    document.getElementById("modelreserva").style.display = "block";
+ }
+
+ function closeModalreserva(){
+    document.getElementById("modelreserva").style.display = "none";   
+ }
+ 
+ function openLoginModal() {
+    document.getElementById('modal').style.display = "block";
+    document.getElementById('modalOverlay').style.display = "block";
+   }
+   
+   function closeLoginModal() {
+    document.getElementById('modal').style.display = "none";
+    document.getElementById('modalOverlay').style.display = "none"; 
+   }
+   
+   
+   function mostrarsolicitacao(){
+      document.getElementById("reserva").style.display = "block";
+      
+   }
+   function closeModalsolicitacao(){
+      document.getElementById("reserva").style.display = "none";   
+   }
---
+++ Sistema-de-Restaurante1/README.md
@@ -0,0 +1,2 @@
+# Sistema de Restaurante1
+ Sistema de Restaurante1
